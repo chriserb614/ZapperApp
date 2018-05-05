130772ddec81143e21e95a9ec671876bd864279d
Marisol G. Martinez <marisol.gabriela.martinez@gmail.com>
2018-05-05 10:09:58 -0700

    QuintonChanges

--- models/critiqueModel.js
+++ models/critiqueModel.js
@@ -1,31 +1,30 @@
-var mongoose = require('mongoose');
-var passportLocalMongoose = require('passport-local-mongoose');
+var mongoose = require('mongoose')
+var passportLocalMongoose = require('passport-local-mongoose')
 
 var CritiqueSchema = new mongoose.Schema({
-    reviewer: String,
-    // reviewer: {
-    //     id: {
-    //         type: mongoose.Schema.Types.ObjectId,
-    //         ref: "User"
-    //     },
-    // },
-    // work: {
-    //     id: {
-    //         type: mongoose.Schema.Types.ObjectId,
-    //         ref: "Work"
-    //     },
-    //     title: String
-    // },
-    critique: String,
-    date: {
-        type: Date,
-        default: Date.now
-    },
-    rating: {
-        type: Number
-    }
-});
+  reviewer: String,
+  // reviewer: {
+  //     id: {
+  //         type: mongoose.Schema.Types.ObjectId,
+  //         ref: "User"
+  //     },
+  // },
+  // work: {
+  //     id: {
+  //         type: mongoose.Schema.Types.ObjectId,
+  //         ref: "Work"
+  //     },
+  //     title: String
+  // },
+  critique: String,
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  rating: {
+    type: Number
+  }
+})
 
-
-CritiqueSchema.plugin(passportLocalMongoose);
-module.exports = mongoose.model("Critique", CritiqueSchema);
+CritiqueSchema.plugin(passportLocalMongoose, {usernameField: 'reviewer'})
+module.exports = mongoose.model('Critique', CritiqueSchema)
