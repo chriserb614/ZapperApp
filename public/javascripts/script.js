6dcade626e9d47609da8e3a4426d1cb7c9203fff
Brett <bseitz754@gmail.com>
2018-05-07 18:35:48 -0700

    filter, pug

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,7 +1,12 @@
 $(document).ready(function() {
 
+    $('#button').click(function(){
+        var selectAgeRange = $('#agerange').val()
+        var selectType = $('#type').val()
+        var selectGenre = $('#genre').val()
+        var selectLength = $('#length').val()
+       
+    })
+    
     console.log("connected")
-    // $('#postArticle').on('click', function() {
-    //     $('#postCard').css('box-shadow', '10px', '10px', '5px', '#888');
-    // });
 });
